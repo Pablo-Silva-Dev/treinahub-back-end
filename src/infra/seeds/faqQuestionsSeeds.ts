49b1987cc1fda550909ca3abe092974d26b70b01
Pablo <pablolucio_@hotmail.com>
2024-08-15 11:01:13 -0300

    feat: adds script to platn seeds

---
+++ src/infra/seeds/faqQuestionsSeeds.ts
@@ -0,0 +1,51 @@
+export const faqQuestionsSeeds = [
+  {
+    question: "Como faço para falar com o suporte?",
+    answer:
+      "Para falar com o suporte basta acessar o Whatsapp de contato do administrador do seu treinamento.",
+  },
+  {
+    question: "Como faço para acompanhar meu progresso?",
+    answer:
+      "Para acompanhar seu progresso, acesse a aba 'Acessar meus treinamentos' no menu esquerdo. Cada treinamento irá exibir o respectivo progresso.",
+  },
+  {
+    question: "Como faço para assistir as videoaulas?",
+    answer:
+      "Para assistir as videoaulas, vá até a aba 'Treinamentos' no menu esquerdo e selecione o treinamento desejado.",
+  },
+  {
+    question: "Qual é o horário de atendimento do suporte?",
+    answer:
+      "O suporte está disponível de segunda a sexta-feira, das 9h às 18h.",
+  },
+  {
+    question: "Como posso redefinir minha senha?",
+    answer:
+      "Para redefinir sua senha, clique em 'Esqueci minha senha' na página de login e siga as instruções.",
+  },
+  {
+    question: "Posso acessar o conteúdo pelo celular?",
+    answer:
+      "Sim, nosso conteúdo é acessível em dispositivos móveis através do navegador ou do aplicativo dedicado.",
+  },
+  {
+    question: "Como faço para emitir um certificado?",
+    answer:
+      "Para emitir um certificado, assista a todas as video aulas do respectivo treinamento que deseja obter o certificado. Após 100% das aulas forem assistidas, clique no botão 'Emitir certificado' do respectivo treinamento.",
+  },
+  {
+    question: "Há algum custo adicional para emitir o certificado?",
+    answer: "Não, a emissão do certificado está inclusa no valor do curso.",
+  },
+  {
+    question: "Como faço para alterar meus dados cadastrais?",
+    answer:
+      "Para alterar seus dados cadastrais, acesse a aba 'Acessar meu perfil' no menu esquerdo, na sequência clique em 'Atualizar dados', edite as informações desejadas. e clique em 'Salvar dados'",
+  },
+  {
+    question: "O que fazer se encontrar um erro no conteúdo?",
+    answer:
+      "Se encontrar um erro no conteúdo, por favor, reporte ao suporte através do Whatsapp ou do e-mail de contato.",
+  },
+];
