9971f02bd9dca020625284f3a440e5d8e63a4339
Pablo <pablolucio_@hotmail.com>
2024-08-27 08:24:46 -0300

    refactor: refactors UpdateTrainingController

--- src/infra/controllers/trainings/updateTrainingController.ts
+++ src/infra/controllers/trainings/updateTrainingController.ts
@@ -1,33 +1,43 @@
-import { IUpdateTrainingDTO } from "@/infra/dtos/TrainingDTO";
+import { ManageFileService } from "@/infra/services/manageFileService";
 import { UpdateTrainingUseCase } from "@/infra/useCases/trainings/updateTrainingUseCase";
 import {
   BadRequestException,
-  Body,
   ConflictException,
   Controller,
   HttpCode,
   Put,
+  Req,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { AuthGuard } from "@nestjs/passport";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { TEnvSchema } from "env";
+import { Request } from "express";
 import { z } from "zod";
 
 const updateTrainingSchema = z.object({
   id: z.string(),
   name: z.string().optional(),
   description: z.string().optional(),
-  cover_url: z.string().url().optional(),
 });
 
 @Controller("/trainings/update")
 @UseGuards(AuthGuard("jwt-admin"))
+@UseInterceptors(FileInterceptor("file"))
 export class UpdateTrainingController {
-  constructor(private updateTrainingUseCase: UpdateTrainingUseCase) {}
+  constructor(
+    private updateTrainingUseCase: UpdateTrainingUseCase,
+    private configService: ConfigService<TEnvSchema, true>,
+    private manageFileService: ManageFileService
+  ) {}
 
   @Put()
   @HttpCode(203)
-  async handle(@Body() body: IUpdateTrainingDTO) {
-    const isBodyValidated = updateTrainingSchema.safeParse(body);
+  async handle(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
+    const isBodyValidated = updateTrainingSchema.safeParse(req.body);
 
     if (!isBodyValidated.success) {
       throw new BadRequestException({
@@ -37,7 +47,23 @@ export class UpdateTrainingController {
     }
 
     try {
-      const updatedTraining = await this.updateTrainingUseCase.execute(body);
+      const fileExtension = file.originalname.split(".")[1];
+      const fileName = req.body.name + "-cover" + fileExtension;
+
+      const blobStorageContainerName = this.configService.get(
+        "AZURE_BLOB_STORAGE_TRAININGS_COVERS_CONTAINER_NAME"
+      );
+
+      const uploadedFile = await this.manageFileService.uploadFile(
+        file.buffer,
+        fileName,
+        blobStorageContainerName
+      );
+
+      const updatedTraining = await this.updateTrainingUseCase.execute({
+        ...req.body,
+        cover_url: uploadedFile,
+      });
       return updatedTraining;
     } catch (error) {
       console.log("[INTERNAL ERROR]", error.message);
