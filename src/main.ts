5db2eefac1d405e52b750e71ebd90a2a325cfe9f
Pablo <pablolucio_@hotmail.com>
2025-10-25 09:31:25 -0300

    fix: fixes ffmpeg and ffprobe usage

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ import { ResponseErrorInterceptor } from "./infra/interceptors/responseError.int
 import { ResponseSuccessInterceptor } from "./infra/interceptors/responseSuccess.interceptor";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
+const ffmpeg = require('fluent-ffmpeg');
+const { path: ffmpegPath } = require('@ffmpeg-installer/ffmpeg');
+const { path: ffprobePath } = require('@ffprobe-installer/ffprobe');
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
+
 async function bootstrap() {
   
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
